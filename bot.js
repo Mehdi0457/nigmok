4e4bedb55a09d76d342ec81fb31a5fc20fed92e2
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-04 14:50:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Mehdi");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("552124874655793172")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`تعال بشبعك حب بشبعك دلال تعال يبن حلال`);
 }, 30)
 })
 
